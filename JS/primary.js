39be517566eec38b9fd56df57eadb2e33f88e048
Matt Tilley <mtilley255@gmail.com>
2017-10-29 15:41:24 -0500

    P31 - practice code completed.

--- JS/primary.js
+++ JS/primary.js
@@ -8,5 +8,31 @@ $(document).ready(function()
         bookings++;
         document.getElementById("DISPLAY").innerHTML = bookings;
         
+        if (bookings > 10)
+        {
+            document.getElementById("DISPLAY").innerHTML = "NO VACANCY";
+        }
+        
     });
+    
+    
+    $("#CANCEL").on("click", function()
+    {
+        if (bookings > 10)
+        {
+            bookings = 10;
+            bookings --;
+            document.getElementById("DISPLAY").innerHTML = bookings;
+        }
+        
+        else
+        {
+            bookings--
+            document.getElementById("DISPLAY").innerHTML = bookings;
+        }
+    })
+    
+    
+    
+    
 });
